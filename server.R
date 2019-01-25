d3d622120e6435f0d1f07213becf9a1facabb373
ptagliolato <ptagliolato@users.noreply.github.com>
2019-01-25 18:10:31 +0100

    Solve file management across sessions

--- server.R
+++ server.R
@@ -9,6 +9,10 @@
 #     Could it affect a multi-user environment?
 
 library(shiny)
+library(raster)
+library(rgdal)
+library(gdalUtils)
+
 options(shiny.maxRequestSize=1000*1024^2) 
 shinyServer(function(input, output, session) {
   
@@ -25,26 +29,29 @@ shinyServer(function(input, output, session) {
         input_dem<-input$file1$datapath
         input_mode<-input$mode
         
-        library(rgdal)
-        library(gdalUtils)
+        temptiff <- tempfile(fileext = ".tif")
         
         # check params depending on input_mode. Cf. https://www.gdal.org/gdaldem.html
         if(input_mode=="hillshade"){
           out <- gdalUtils::gdaldem(mode=input_mode,
                                     input_dem=input_dem, 
-                                    output=paste("out_",input_mode,".tif", sep=""),
+                                    output=temptiff,
                                     output_Raster=TRUE,verbose=TRUE, z=input$z, az=input$az, alt=input$alt
           )
+          
           #browser()
         }
         else{
           out <- gdalUtils::gdaldem(mode=input_mode,
-                                    input_dem=input_dem, 
-                                    output=paste("out_",input_mode,".tif", sep=""),
+                                  input_dem=input_dem, 
+                                    output=temptiff,
                                     output_Raster=TRUE,verbose=TRUE
           )
+          
         }
         })#end progress
+        
+        #browser()
         return(out)
         
       })#end isolate
@@ -54,7 +61,7 @@ shinyServer(function(input, output, session) {
   
   currentInputRaster<-reactive({
     req(input$file1)
-    library(raster)
+    
     input_dem<-input$file1$datapath
     return(raster(input_dem))
   })
@@ -63,102 +70,28 @@ shinyServer(function(input, output, session) {
   
   observeEvent(currentProcessedRaster(),{enable("downloadData")})
   
+  #observe({output$path<-renderUI(textInput("MYPATH", "path", value = req(input$file1$datapath)))})
   
+  # plot input raster
   output$mapInput<-renderPlot({
-    # req(input$file1)
-    # library(raster)
-    # input_dem<-input$file1$datapath
-    # plot(raster(input_dem),col=gray.colors(256))
     withProgress(message = 'Making plot...', value = 0.5,{
       plot(currentInputRaster(),col=gray.colors(256))
     })
   })
   
-  output$mapImg<-renderPlot({
-    if(input$doPlotMap!=0){
-      isolate(
-        withProgress(message = 'Making plot...', value = 0.5,{
-          plot(currentProcessedRaster(),col=gray.colors(256))
-        })
-      )
-    }
+  #on button click process input raster and plot it
+  observeEvent(input$doPlotMap, ignoreInit = TRUE, {
+    r <- currentProcessedRaster()
+    #browser()
+    output$mapImg<-renderPlot(withProgress(message = 'Making plot...', value = 0.5,{
+      plot(r,col=gray.colors(256))
+    }))
   })
-  # TODO: anticipate rendering input map before processed output.
-  # Consider reactive value/action button to process the image on user request.
-  # output$mapImg <- renderPlot({
-  #   
-  #   #alg ZevenbergenThorne | Horn
-  #   #z: vertical exaggeration
-  #   #file
-  #   #
-  #   # generate bins based on input$bins from ui.R
-  #   #x    <- faithful[, 2]
-  #   #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-  #   
-  #   # draw the histogram with the specified number of bins
-  #   #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-  #   
-  #   #......
-  #   req(input$file1)
-  #   
-  #   input_dem<-input$file1$datapath
-  #   input_mode<-input$mode
-  #   
-  #   if(FALSE){
-  #     #code for hillshade
-  #     tif_file = raster::raster(input_dem)
-  #     
-  #     withProgress(message = 'Making plot', value = 0,{
-  #       #And convert it to a matrix:
-  #       elmat = matrix(raster::extract(tif_file,raster::extent(tif_file),buffer=1000),
-  #                      nrow=ncol(tif_file),ncol=nrow(tif_file))
-  #       
-  #       #We first texture the map with sphere_shade and one of rayshader's built in textures, "desert."
-  #       #By default, the highlight is towards the NW.
-  #       #rgbarray<-
-  #       elmat %>%
-  #         sphere_shade(texture = "desert") %>%
-  #         plot_map()
-  #     })
-  #   }
-  #   
-  #   else{
-  #     
-  #     library(rgdal)
-  #     library(gdalUtils)
-  #     
-  #     #gdalUtils::gdaldem("hillshade",)
-  #     
-  #     #plot(raster(input_dem),col=gray.colors(256))
-  #     
-  #     withProgress(message = 'Making plot', value = 0.5,{
-  #       
-  #       # check params depending on input_mode. Cf. https://www.gdal.org/gdaldem.html
-  #       if(input_mode=="hillshade"){
-  #         output_hillshade <- gdalUtils::gdaldem(mode=input_mode,
-  #                                                input_dem=input_dem, 
-  #                                                output=paste("output_",input_mode,".tif", sep=""),
-  #                                                output_Raster=TRUE,verbose=TRUE, z=input$z, az=input$az, alt=input$alt
-  #         )
-  #       }
-  #       else{
-  #         output_hillshade <- gdalUtils::gdaldem(mode=input_mode,
-  #                                                input_dem=input_dem, 
-  #                                                output=paste("out_",input_mode,".tif", sep=""),
-  #                                                output_Raster=TRUE,verbose=TRUE
-  #         )
-  #       }
-  #       
-  #       plot(output_hillshade,col=gray.colors(256))
-  #     })#end progress
-  #   }
-  # })
-  
+
   output$downloadData<-downloadHandler(filename=paste("output_",input$mode,".tif", sep=""), 
                                        content=function(file){
-                                         #writeRaster(currentProcessedRaster(),file)
-                                         browser()
-                                         file.copy(paste("out_",input$mode,".tif", sep=""), file)
+                                         writeRaster(currentProcessedRaster(),file, datatype = "INT1U", options=c("COMPRESSION=DEFLATE"))
+                                         # browser()
                                        }
   )
 })
--- ui.R
+++ ui.R
@@ -26,24 +26,9 @@ dashboardPagePlus(
       tags$img(src = "http://tellmehub.get-it.it/static/img/logo1_200px.png", width = "100px", height = "50px")
     )
     # fixed = FALSE,
-     ,enable_rightsidebar = TRUE
+    # ,enable_rightsidebar = TRUE
     # rightSidebarIcon = "gears",
-    # tags$li(class ="dropdown", 
-    #         tags$a(
-    #           href="http://www.lter-europe.net",
-    #           tags$img(src="http://www.get-it.it/assets/img/loghi/eLTERH2020.png"),
-    #           style="margin:0;padding-top:2px;padding-bottom:2px;padding-left:10px;padding-right:10px;",
-    #           target="_blank"
-    #         )
-    # ),
-    # tags$li(class ="dropdown", 
-    #         tags$a(
-    #           href="http://www.lteritalia.it",
-    #           tags$img(src="http://www.get-it.it/assets/img/loghi/LogoLTERIta.png"),
-    #           style="margin:0;padding-top:2px;padding-bottom:2px;padding-left:10px;padding-right:10px;",
-    #           target="_blank"
-    #         )
-    # )
+    
   ),
   
   dashboardSidebar(
@@ -104,7 +89,7 @@ dashboardPagePlus(
                               min = 0,
                               max = 90,
                               value = 45),
-                  
+                  #uiOutput("path"),
                   height = "650px", style = "padding-left: 10px;padding-right: 10px;"
                 ),
                 fluidRow(
